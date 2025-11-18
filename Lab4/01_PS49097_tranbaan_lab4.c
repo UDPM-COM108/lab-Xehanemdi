7995f3f0e0ac73f9207ee145640bc8ceb14dfead
Tran Ba An <tranbaan2004@gmail.com>
2025-11-18 16:09:48 +0700

    demoslidelab4

---
+++ Lab4/01_PS49097_tranbaan_lab4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+int min, max;
+int i = min ;
+float tong = 0, bienDiem = 0, trungbinh = 0, x ;
+/*
+printf("nhap gia tri min:");
+scanf("%d",&min);
+printf("nhap gia tri max:");
+scanf("%d", &max);
+    while(i<= max){
+        if(i%2==0){
+            tong+=i;
+            bienDiem++;
+        }
+        i++;
+    }
+    trungbinh = (tong/bienDiem) ;
+printf("trung binh tong cua cac so tu nhien chia het cho 2: %.2f",  trungbinh );
+*/
+printf("nhap so x de xet nghiem:");
+scanf("%d",)
+    return 0;
+}
